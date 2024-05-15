632e2dbf664220faf24840f64ef6fcbcc9a7101c
Latoya <76912011+latoyaln@users.noreply.github.com>
2024-05-15 12:20:57 +0200

    unused code deleted

--- server.js
+++ server.js
@@ -161,58 +161,10 @@ app.post('/', async (req, res) => {
   });
 });
 
-
-
 //? Tijdelijke code uitgezet
 app.get('/playlists', async (request, response) => {
-  // try {
-  //   const API =  `${apiUrl}/tm_playlist`;
-  //   const [data] = await Promise.all([
-  //     fetch(API).then(res => res.json()),
-  //   ]);
-  //   response.render('playlists', {
-  //     playlist: dataFinal.data,
-  //   });
-  // } catch (error) {
-  //   console.error(error);
-  //   response.status(500).send("Internal Server Error");
-  // }
+
 });
-
-// // Renderd de playlist pagina via de slug
-// app.get('/:slug', async (request, response) => {
-//   // try {
-//   //   const API = `${apiUrl}/tm_playlist?filter={"slug":"${request.params.slug}"}&fields=title,description,slug,stories.tm_story_id.title,stories.tm_story_id.summary,stories.tm_story_id.image,stories.tm_story_id.slug,language_id.language,language_id.flag.id`;
-//   //   const [data] = await Promise.all([
-//   //     fetch(API).then(res => res.json()),
-//   //   ]);
-//   //   response.render('playlist', {
-//   //     playlist: dataFinal.data[0],
-//   //     stories: dataFinal.data[0].stories || [],
-//   //     language: dataFinal.data[0].language_id || [],
-//   //   });
-//   // } catch (error) {
-//   //   console.error(error);
-//   //   response.status(500).send("Internal Server Error");
-//   // }
-// });
-
-// // Renderd de story pagina via de slug
-// app.get('/:playlistSlug/:storySlug', async (request, response) => {
-//   // try {
-//   //   const API = `${apiUrl}/tm_story?filter={"slug":"${request.params.storySlug}"}&fields=title,description,slug,image,video,playlist.tm_playlist_id.title,playlist.tm_playlist_id.slug, playlist.tm_playlist_id.description,`;
-//   //   const [data] = await Promise.all([
-//   //     fetch(API).then(res => res.json()),
-//   //   ]);
-//   //   response.render('story', {
-//   //     story: dataFinal.data[0],
-//   //   });
-//   // } catch (error) {
-//   //   console.error(error);
-//   //   response.status(500).send("Internal Server Error");
-//   // }
-// });
-
 
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000)
