88d76fce83c6a70c48358a4afe893188fc38bb3c
Jelte (aka Driezie) <80174866+driezie@users.noreply.github.com>
2024-05-17 12:11:54 +0200

    #14 Console logs weggehaald
    Console logs weggehaald die niet nodig zijn

--- helpers/fetchManager.js
+++ helpers/fetchManager.js
@@ -7,22 +7,12 @@ export async function makeFetchRequest(url, method, body = null) {
       'Content-Type': 'application/json' // Geef aan dat de request body JSON is
     }
   };
-
   // Als er een body is, voeg deze dan toe aan het opties object na het "stringiferen"
   if (body) {
     options.body = JSON.stringify(body);
   }
-
   // Doe het fetch verzoek met de opgegeven URL en opties
   const response = await fetch(url, options);
-
-  // Controleer of de response niet OK is
-  if (!response.ok) {
-    // Gooi een fout met een bericht dat de methode en URL aangeeft die mislukt is
-    throw new Error(`Failed to ${method} at ${url}`);
-  } else {
-    // Log dat het verzoek succesvol was
-    console.log(`Successfully used ${method} request at ${url}`);
-  }
+  // Geef het response terug
   return response;
 }
