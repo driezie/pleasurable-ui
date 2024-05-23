e111507c25072ca515b1abfa30e649cc3fcc5dc6
RukiyaTossou <114156045+RukiyaTossou@users.noreply.github.com>
2024-05-23 23:07:07 +0200

    img toegevoegd

--- public/main.js
+++ public/main.js
@@ -12,34 +12,31 @@ document.addEventListener('DOMContentLoaded', function() {
         storiesButton.addEventListener('click', function() {
 
     });
-`z`
+
     } else {
         console.error('One or more elements are not found in the DOM.');
     }
 
-    // active state 
-   
-// Function to toggle active state between buttons
-function toggleActiveButton(clickedButton, otherButton) {
-    // Check if the clicked button already has the active class
-    if (!clickedButton.classList.contains('active')) {
-        // Remove active state from the other button
-        otherButton.classList.remove('active');
-        
-        // Add active class to the clicked button
-        clickedButton.classList.add('active');
-    }
-}
+            
+        storiesButton.addEventListener('click', function() {
+            toggleActiveButton(storiesButton, playlistsButton);
+        });
 
-// Event listener for the stories button
-storiesButton.addEventListener('click', function() {
-    toggleActiveButton(storiesButton, playlistsButton);
-});
+        playlistsButton.addEventListener('click', function() {
+            toggleActiveButton(playlistsButton, storiesButton);
+        }); 
 
-// Event listener for the playlists button
-playlistsButton.addEventListener('click', function() {
-    toggleActiveButton(playlistsButton, storiesButton);
-}); 
+
+ // active state 
+    function toggleActiveButton(clickedButton, otherButton) {
+        //checken of er al een active state is 
+        if (!clickedButton.classList.contains('active')) {
+            // active state weghalen 
+            otherButton.classList.remove('active');
+            
+            // Active state toevoegen
+            clickedButton.classList.add('active');
+        }
 
     // stories laten zien 
     function showStories() {
@@ -51,4 +48,4 @@ playlistsButton.addEventListener('click', function() {
         playlistsContent.classList.remove('hidden');
         storiesContent.classList.add('hidden');
     }
-});
+}});
