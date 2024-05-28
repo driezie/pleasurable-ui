e8bb4653e515320887c15bf9a0e162f87e78ca0f
RukiyaTossou <114156045+RukiyaTossou@users.noreply.github.com>
2024-05-28 12:34:34 +0200

    feedback verwerkt in de stories

--- public/main.js
+++ public/main.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const storiesContent = document.querySelector('.list-stories');
     const playlistsContent = document.querySelector('.list-playlists');
 
+    const hamburgerButton = document.querySelector('.menu-button');
+    const menu = document.querySelector('.menu-container');
+    const closeIcon = document.querySelector('.menu-close-button');
+
     // Check of ze bestaan
     if (storiesButton && playlistsButton && storiesContent && playlistsContent) {
         storiesButton.addEventListener('click', showStories);
@@ -16,8 +20,6 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         console.error('One or more elements are not found in the DOM.');
     }
-
-            
         storiesButton.addEventListener('click', function() {
             toggleActiveButton(storiesButton, playlistsButton);
         });
@@ -25,6 +27,27 @@ document.addEventListener('DOMContentLoaded', function() {
         playlistsButton.addEventListener('click', function() {
             toggleActiveButton(playlistsButton, storiesButton);
         }); 
+
+     // menu 
+
+    // hamburgerButton.addEventListener("click", toggleMenu);
+    // closeIcon.addEventListener("click", toggleMenu);
+
+        
+    // function toggleMenu() {
+    //     console.log("toggle check");
+    //     // Toggle de klasse 'show-menu'
+    //     menu.classList.toggle("show-menu");
+    
+    //     // Check of 'show-menu' class er is 
+    //     if (menu.classList.contains("show-menu")) {
+    //         // als 'showMenu' class er is:  display block
+    //         menu.style.display = "block";
+    //     } else {
+    //          // als 'showMenu' class er niet is:  display none
+    //         menu.style.display = "none";
+    //     }
+    // }
 
 
 //  // active state 
